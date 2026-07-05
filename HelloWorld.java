aa97dba79dd4c63c0bfc3e7e4fb92dd7534a1c33
Suemon-Kwok <149294826+Suemon-Kwok@users.noreply.github.com>
2026-07-05 16:27:31 +1200

    Code added

---
+++ HelloWorld.java
@@ -0,0 +1,31 @@
+/*Object oriented programming Lab 1 question 2
+
+Complete the main method in the HelloWorld class to output the string Hello World!
+
+For example:
+Test	Result
+HelloWorld.main(new String[]{})
+
+	Hello World!
+
+	
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        //output Hello World! Here
+}
+
+outputting text in Java
+System.out.println()
+*/
+
+
+public class HelloWorld 
+{
+	public static void main(String[] args)
+	{		
+			//Out Hello World!" to Console
+			System.out.println("Hello World!");
+	}
+}	
